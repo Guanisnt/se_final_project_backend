f2f9291731821ace83852d1cebafe1cd30df7449
Guanisnt <allen6010101@gmail.com>
2025-05-02 20:21:05 +0800

    統一用db_connect.php連線、修改api層級

--- backend/api/auth/login.php
+++ backend/api/auth/login.php
@@ -1,12 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
 header("Content-Type: application/json");
-
 $input = json_decode(file_get_contents("php://input"), true);
-
-$conn = new mysqli("localhost", "root", "12345678", "se_final_project");
+require_once '../db_connect.php';
 
 if ($conn->connect_error) {
     echo json_encode(["success" => false, "error" => "資料庫連線失敗"]);
