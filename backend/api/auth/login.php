b30ced2230f3c5909d26feb2620da61920e3d25d
puhua0210 <puhua0210@gmail.com>
2025-05-03 14:36:25 +0800

    login完成

--- backend/api/auth/login.php
+++ backend/api/auth/login.php
@@ -11,5 +11,30 @@ if ($conn->connect_error) {
     exit;
 }
 
+$uid= $input["id"];
+$password= $input["password"];
+
+$sql = "SELECT password FROM users WHERE uid = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $uid);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($row = $result->fetch_assoc()) { //是否有帳號
+    if ($row["password"] === $password) { //驗證密碼
+        echo json_encode(["success" => true]);
+    } else { //密碼錯誤
+        echo json_encode([
+            "success" => false,
+            "error" => "帳號或密碼錯誤"
+        ]);
+    }
+} else { //帳號錯誤
+    echo json_encode([
+        "success" => false,
+        "error" => "帳號或密碼錯誤"
+    ]);
+}
+$stmt->close();
 $conn->close();
 ?>
