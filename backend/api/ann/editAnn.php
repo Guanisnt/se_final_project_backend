7c7bb8bd2bd76e3334cccc9b59d836935f2340dc
Guanisnt <allen6010101@gmail.com>
2025-05-21 22:33:33 +0800

    修改公告API完成

---
+++ backend/api/ann/editAnn.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Content-Type: application/json");
+$input = json_decode(file_get_contents("php://input"), true);
+require_once '../db_connect.php';
+
+if ($conn->connect_error) {
+    echo json_encode(["success" => false, "error" => "資料庫連線失敗"]);
+    exit;
+} 
+
+echo json_encode([
+    "success" => true,
+    // "input" => $input,
+    // "aId" => $input['aId'],
+    // "annTitle" => $input['annTitle'],
+    // "annContent" => $input['annContent'],
+    // "uId" => $input['uId'],
+]);
+
+?>
