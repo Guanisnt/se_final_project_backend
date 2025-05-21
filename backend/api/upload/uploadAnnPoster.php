c4f33de29eb16a5fe142770d7ba6164f8470a658
Guanisnt <allen6010101@gmail.com>
2025-05-21 21:06:54 +0800

    新增公告poster小修

--- backend/api/upload/uploadAnnPoster.php
+++ backend/api/upload/uploadAnnPoster.php
@@ -4,7 +4,7 @@ if (!file_exists($targetDir)) {
     mkdir($targetDir, 0777, true);
 }
 
-$uId = isset($_POST['uId']) ? $_POST['uId'] : null;
+$aId = isset($_POST['aId']) ? $_POST['aId'] : null;
 
 if (isset($_FILES["file"])) {
     $fileName = basename($_FILES["file"]["name"]);
@@ -14,7 +14,8 @@ if (isset($_FILES["file"])) {
         $imageUrl = "http://se_final_project_backend.local:8081/api" . str_replace("../", "/", $targetFilePath);
         echo json_encode([
             "success" => true,
-            "imgUrl" => $imageUrl
+            // "imgUrl" => $imageUrl,
+            // "aId" => $aId
         ]);
     } else {
         echo json_encode(["success" => false, "error" => "Upload failed."]);
