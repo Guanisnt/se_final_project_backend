08758703daefa679fe32664ca339554381d09ff3
Guanisnt <allen6010101@gmail.com>
2025-05-21 22:52:15 +0800

    刪除公告posterAPI完成

---
+++ backend/api/ann/deleteAnnFile.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Content-Type: application/json");
+require_once '../db_connect.php';
+
+if ($conn->connect_error) {
+    echo json_encode(["success" => false, "error" => "資料庫連線失敗"]);
+    exit;
+} 
+
+$aId = isset($_GET['aId']) ? intval($_GET['aId']) : null;
+$posterUrl= isset($_GET['posterUrl']) ? $_GET['posterUrl'] : null;
+
+echo json_encode([
+    "success" => true,
+    "aId" => $aId,
+    "posterUrl" => $posterUrl,
+]);
+/* === 從這邊以下開始寫資料庫操作 === */
+?>
