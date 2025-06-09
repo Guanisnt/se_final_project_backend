262240d375beddea93a05a0460efefbc26302058
Guanisnt <allen6010101@gmail.com>
2025-06-09 11:26:25 +0800

    刪除學生證API

---
+++ backend/api/competition/deleteStudentCard.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Content-Type: application/json");
+require_once '../db_connect.php';
+
+if ($conn->connect_error) {
+    echo json_encode(["success" => false, "error" => "資料庫連線失敗"]);
+    exit;
+} 
+
+$uId = isset($_GET['uId']) ? $_GET['uId'] : null;
+
+$conn->close();
+?>
